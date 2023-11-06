d420a301213476c65529322fd95b526826f271b0
Vini Black <viniibl4ck@gmail.com>
2023-11-06 17:28:46 -0300

    finalizando o CRUD

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
 router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criarPessoa);
+router.put("/pessoas/:id", PessoaController.atualizarPessoa);
+router.delete("/pessoas/:id", PessoaController.apagarPessoa);
 
 module.exports = router;
