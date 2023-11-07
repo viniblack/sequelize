9e0dfc9d8395badb60867e00172b57de242ba4e7
Vini Black <viniibl4ck@gmail.com>
2023-11-07 15:38:45 -0300

    Criando validação

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -14,9 +14,22 @@ module.exports = (sequelize, DataTypes) => {
   }
   Pessoas.init(
     {
-      nome: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          funcaoValidadora: function (dado) {
+            if (dado.length < 3)
+              throw new Error("campo deve ter mais que 3 caracteres");
+          },
+        },
+      },
       ativo: DataTypes.BOOLEAN,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: { args: true, msg: "dado do tipo e-mail inválido" },
+        },
+      },
       role: DataTypes.STRING,
     },
     {
